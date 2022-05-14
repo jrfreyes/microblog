47745dcdaab04f5eaaba279374146a8248a40bfb
jrfreyes <justinrupert.reyes@gmail.com>
2022-05-14 19:10:18 +0900

    Complete Chapter 12

--- app/__init__.py
+++ app/__init__.py
@@ -2,12 +2,13 @@ import logging
 import os
 from logging.handlers import RotatingFileHandler, SMTPHandler
 
-from flask_bootstrap import Bootstrap
 from config import Config
 from flask import Flask
+from flask_bootstrap import Bootstrap
 from flask_login import LoginManager
 from flask_mail import Mail
 from flask_migrate import Migrate
+from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -18,6 +19,7 @@ login = LoginManager(app)
 login.login_view = 'login'
 mail = Mail(app)
 bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 from app import errors, models, routes
 
