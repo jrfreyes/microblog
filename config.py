7b5d166a8e7928ccec85049e6075b520278d13e6
jrfreyes <justinrupert.reyes@gmail.com>
2022-05-17 15:48:26 +0900

    heroku deployment changes

--- config.py
+++ config.py
@@ -7,7 +7,8 @@ load_dotenv(os.path.join(basedir, '.env'))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace(
+        'postgres://', 'postgresql://') if os.environ.get('DATABASE_URL') else\
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
@@ -23,3 +24,4 @@ class Config(object):
     ELASTICSEARCH_CERT = os.environ.get('ELASTICSEARCH_CERT')
     ELASTICSEARCH_USER = os.environ.get('ELASTICSEARCH_USER')
     ELASTICSEARCH_PASS = os.environ.get('ELASTICSEARCH_PASS')
+    LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
